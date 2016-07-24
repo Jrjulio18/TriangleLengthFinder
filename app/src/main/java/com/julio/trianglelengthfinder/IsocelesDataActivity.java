5e815a906f8fa5c4cbb5ac88cc0fcdd6e45066a5
jrjulio18@gmail.com <18Cesar95@>
2016-07-23 18:14:21 -0700

    All the check boxes are working fine Maybe more conditions can be implemented so the app wont crash!!!

--- app/src/main/java/com/julio/trianglelengthfinder/IsocelesDataActivity.java
+++ app/src/main/java/com/julio/trianglelengthfinder/IsocelesDataActivity.java
@@ -3,6 +3,7 @@ package com.julio.trianglelengthfinder;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,7 +12,7 @@ import java.text.DecimalFormat;
 
 public class IsocelesDataActivity extends AppCompatActivity {
     static int chooser = 0;
-    static int checker= 0;
+
     CheckBox checkBoxHeight, checkBoxBase, checkBoxAngle;
 
 
@@ -67,30 +68,204 @@ public class IsocelesDataActivity extends AppCompatActivity {
             //The key argument here must match that used in the other activity
         }
     }
-    public void checkOne(View view){
-        if(!checkBoxHeight.isChecked()){
+    public void checkOne(View view) {
+        Button dataResult = (Button) findViewById(R.id.isoButtonLength);
+        if (!checkBoxHeight.isChecked()) {
             EditText txtOne = (EditText) findViewById(R.id.isoFirstData);
             txtOne.setVisibility(View.GONE);
         }
-        if (!checkBoxBase.isChecked()){
+        if (!checkBoxBase.isChecked()) {
             EditText txtTwo = (EditText) findViewById(R.id.isoSecondData);
             txtTwo.setVisibility(View.GONE);
         }
-        if (!checkBoxAngle.isChecked()){
+        if (!checkBoxAngle.isChecked()) {
             EditText txtThree = (EditText) findViewById(R.id.isoThirdData);
             txtThree.setVisibility(View.GONE);
         }
-        if(checkBoxHeight.isChecked()){
+        if (checkBoxHeight.isChecked()) {
             EditText txtOne = (EditText) findViewById(R.id.isoFirstData);
             txtOne.setVisibility(View.VISIBLE);
         }
-        if (checkBoxBase.isChecked()){
+        if (checkBoxBase.isChecked()) {
             EditText txtTwo = (EditText) findViewById(R.id.isoSecondData);
             txtTwo.setVisibility(View.VISIBLE);
         }
-        if (checkBoxAngle.isChecked()){
+        if (checkBoxAngle.isChecked()) {
             EditText txtThree = (EditText) findViewById(R.id.isoThirdData);
             txtThree.setVisibility(View.VISIBLE);
+        }
+        if (checkBoxHeight.isChecked() && checkBoxBase.isChecked() && chooser == 1) {
+
+            dataResult.setOnClickListener(
+
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            EditText txtOne = (EditText) findViewById(R.id.isoFirstData);
+                            Double dataOne =Double.parseDouble(txtOne.getText().toString());
+                            EditText txtTwo = (EditText) findViewById(R.id.isoSecondData);
+                            Double dataTwo =Double.parseDouble(txtTwo.getText().toString());
+                            if( dataOne <= dataTwo){
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                textViewResult.setText("Unvaluable Data");
+                            }
+                            else {
+                                Double base = 2 * (Math.sqrt(Math.pow(dataOne, 2) - Math.pow(dataTwo, 2)));
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                DecimalFormat df = new DecimalFormat("#.####");
+                                Double trimDouble = Double.parseDouble(df.format(base));
+                                textViewResult.setText(Double.toString(trimDouble));
+                                TextView heightDisplay = (TextView) findViewById(R.id.lengthDisplay);
+                                heightDisplay.setText("Base Size:");
+                            }
+                        }
+                    }
+            );
+
+        }
+
+        if (checkBoxHeight.isChecked() && checkBoxAngle.isChecked() && chooser == 1) {
+
+            dataResult.setOnClickListener(
+
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            EditText txtOne = (EditText) findViewById(R.id.isoFirstData);
+                            Double dataOne =Double.parseDouble(txtOne.getText().toString());
+                            EditText txtThree = (EditText) findViewById(R.id.isoThirdData);
+                            Double dataThree =Double.parseDouble(txtThree.getText().toString());
+                            if( dataThree >= 180){
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                textViewResult.setText("Not a possible Triangle");
+                            }
+                            else {
+                                Double base = 2 * dataOne * Math.cos(Math.toRadians(dataThree));
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                DecimalFormat df = new DecimalFormat("#.####");
+                                Double trimDouble = Double.parseDouble(df.format(base));
+                                textViewResult.setText(Double.toString(trimDouble));
+                                TextView heightDisplay = (TextView) findViewById(R.id.lengthDisplay);
+                                heightDisplay.setText("Base Size");
+                            }
+                        }
+                    }
+            );
+
+        }
+
+        if (checkBoxBase.isChecked() && checkBoxAngle.isChecked() && chooser == 1) {
+
+            dataResult.setOnClickListener(
+
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            EditText txtTwo = (EditText) findViewById(R.id.isoSecondData);
+                            Double dataTwo =Double.parseDouble(txtTwo.getText().toString());
+                            EditText txtThree = (EditText) findViewById(R.id.isoThirdData);
+                            Double dataThree =Double.parseDouble(txtThree.getText().toString());
+                            if( dataThree >= 180){
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                textViewResult.setText("Not a possible Triangle");
+                            }
+                            else {
+                                Double base = 2 * dataTwo / (Math.tan(Math.toRadians(dataThree)));
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                DecimalFormat df = new DecimalFormat("#.####");
+                                Double trimDouble = Double.parseDouble(df.format(base));
+                                textViewResult.setText(Double.toString(trimDouble));
+                                TextView heightDisplay = (TextView) findViewById(R.id.lengthDisplay);
+                                heightDisplay.setText("Base Size:");
+                            }
+                        }
+                    }
+            );
+
+        }
+
+        if (checkBoxHeight.isChecked() && checkBoxBase.isChecked() && chooser == 3) {
+
+            dataResult.setOnClickListener(
+
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            EditText txtOne = (EditText) findViewById(R.id.isoFirstData);
+                            Double dataOne =Double.parseDouble(txtOne.getText().toString());
+                            EditText txtTwo = (EditText) findViewById(R.id.isoSecondData);
+                            Double dataTwo =Double.parseDouble(txtTwo.getText().toString());
+
+
+                            Double leg = (Math.sqrt(Math.pow(dataOne, 2) + Math.pow((dataTwo/2), 2)));
+                            TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                            DecimalFormat df = new DecimalFormat("#.####");
+                            Double trimDouble = Double.parseDouble(df.format(leg));
+                            textViewResult.setText(Double.toString(trimDouble));
+                            TextView heightDisplay = (TextView) findViewById(R.id.lengthDisplay);
+                            heightDisplay.setText("Leg Size:");
+                        }
+                    }
+            );
+
+        }
+
+        if (checkBoxHeight.isChecked() && checkBoxAngle.isChecked() && chooser == 3) {
+
+            dataResult.setOnClickListener(
+
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            EditText txtOne = (EditText) findViewById(R.id.isoFirstData);
+                            Double dataOne =Double.parseDouble(txtOne.getText().toString());
+                            EditText txtThree = (EditText) findViewById(R.id.isoThirdData);
+                            Double dataThree =Double.parseDouble(txtThree.getText().toString());
+                            if( dataOne!=0 && dataThree >= 180){
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                textViewResult.setText("Not a possible Triangle");
+                            }
+                            else {
+                                Double leg = dataOne / (Math.sin(Math.toRadians(dataThree)));
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                DecimalFormat df = new DecimalFormat("#.####");
+                                Double trimDouble = Double.parseDouble(df.format(leg));
+                                textViewResult.setText(Double.toString(trimDouble));
+                            }
+                        }
+                    }
+            );
+
+        }
+
+        if (checkBoxBase.isChecked() && checkBoxAngle.isChecked() && chooser == 3) {
+
+            dataResult.setOnClickListener(
+
+                    new View.OnClickListener() {
+                        @Override
+                        public void onClick(View view) {
+                            EditText txtTwo = (EditText) findViewById(R.id.isoSecondData);
+                            Double dataTwo =Double.parseDouble(txtTwo.getText().toString());
+                            EditText txtThree = (EditText) findViewById(R.id.isoThirdData);
+                            Double dataThree =Double.parseDouble(txtThree.getText().toString());
+                            if(dataTwo != 0 && dataThree >= 180){
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                textViewResult.setText("Not a possible Triangle");
+                            }
+                            else {
+                                Double base = dataTwo / (Math.cos(Math.toRadians(dataThree)));
+                                TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
+                                DecimalFormat df = new DecimalFormat("#.####");
+                                Double trimDouble = Double.parseDouble(df.format(base));
+                                textViewResult.setText(Double.toString(trimDouble));
+                                TextView heightDisplay = (TextView) findViewById(R.id.lengthDisplay);
+                                heightDisplay.setText("Base Size:");
+                            }
+                        }
+                    }
+            );
+
         }
     }
 
@@ -104,24 +279,6 @@ public class IsocelesDataActivity extends AppCompatActivity {
                 EditText txtTwo = (EditText) findViewById(R.id.isoSecondData);
                 Double dataTwo =Double.parseDouble(txtTwo.getText().toString());
 
-                if(chooser == 1){
-                    EditText txtThree = (EditText) findViewById(R.id.isoThirdData);
-                    Double dataThree =Double.parseDouble(txtThree.getText().toString());
-                    if(dataTwo != 0 && (dataThree == 0 || dataThree == null)) {
-                        Double base = 2 * (Math.sqrt(Math.pow(dataOne, 2) - Math.pow(dataTwo, 2)));
-                        TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
-                        DecimalFormat df = new DecimalFormat("#.####");
-                        Double trimDouble = Double.parseDouble(df.format(base));
-                        textViewResult.setText(Double.toString(trimDouble));
-                    }
-                    if(dataThree != 0 && (dataTwo == 0 || dataTwo == null)) {
-                        Double base = 2 *dataOne *  Math.cos(Math.toRadians(dataThree));
-                        TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
-                        DecimalFormat df = new DecimalFormat("#.####");
-                        Double trimDouble = Double.parseDouble(df.format(base));
-                        textViewResult.setText(Double.toString(trimDouble));
-                    }
-                }
 
                 if(chooser == 2 ){
                     TextView heightDisplay = (TextView)findViewById(R.id.lengthDisplay);
@@ -133,24 +290,7 @@ public class IsocelesDataActivity extends AppCompatActivity {
                     textViewResult.setText(Double.toString(trimDouble));
                 }
 
-                if(chooser == 3) {
-                    EditText txtThree = (EditText) findViewById(R.id.isoThirdData);
-                    Double dataThree =Double.parseDouble(txtThree.getText().toString());
-                    if(dataTwo != 0 && (dataThree == 0 || dataThree == null)) {
-                        Double leg = (Math.sqrt(Math.pow(dataOne, 2) + Math.pow(dataTwo, 2)));
-                        TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
-                        DecimalFormat df = new DecimalFormat("#.####");
-                        Double trimDouble = Double.parseDouble(df.format(leg));
-                        textViewResult.setText(Double.toString(trimDouble));
-                    }
-                    if(dataThree != 0 && (dataTwo == 0 || dataTwo == null)) {
-                        Double leg = dataOne / (Math.sin(Math.toRadians(dataThree)));
-                        TextView textViewResult = (TextView) findViewById(R.id.isoResultData);
-                        DecimalFormat df = new DecimalFormat("#.####");
-                        Double trimDouble = Double.parseDouble(df.format(leg));
-                        textViewResult.setText(Double.toString(trimDouble));
 
-                    }
                 }
             }
         }
@@ -158,4 +298,4 @@ public class IsocelesDataActivity extends AppCompatActivity {
 
 
 
-}
+
